af93911479897cbefe6760acb2063fccb5094c6a
Ibrahim <mdibrahim.bd320@gmail.com>
2022-11-05 11:24:28 +0600

    add

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
-// import {cityList} from "./city.js";
+import { cityList } from "./city.js";
 
-// cityList.map((item,i)=>{
-//     let Details=`My city name is ${item.name} and code ${item.code}`
-//     console.log(Details)
-// })
 
-import {cityList} from "./city.js";
-
-cityList.map((items,i)=>{
-    let showResult = `My city name is ${items.name} and code no ${items.code}`
-    console.log(showResult);
+cityList.map((city,i)=>{
+    const output = `My city name is ${city.name} and code number ${city.code}`;
+    console.log(output);
 })
+
