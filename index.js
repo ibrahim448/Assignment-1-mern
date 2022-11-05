896cc698ca06b4dfc22ffc81134d1b21f09bd349
Ibrahim <mdibrahim.bd320@gmail.com>
2022-11-05 13:28:07 +0600

    add all

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 import { cityList } from "./city.js";
 
-
 cityList.map((city,i)=>{
-    const output = `My city name is ${city.name} and code number ${city.code}`;
-    console.log(output);
+    const cityShow = `My city name is ${city.name} and code number ${city.code}`;
+    console.log(cityShow);
 })
-
