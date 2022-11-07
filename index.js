652e1df1e5ad1e5417cb133189e86446a24c2403
Ibrahim <mdibrahim.bd320@gmail.com>
2022-11-07 17:36:55 +0600

    add all

--- index.js
+++ index.js
@@ -3,4 +3,22 @@ import { cityList } from "./city.js";
 cityList.map((city,i)=>{
     const cityShow = `My city name is ${city.name} and code number ${city.code}`;
     console.log(cityShow);
-})
+});
+
+
+// for loop use
+/*
+ for(let i = 0; i<cityList.length; i++){
+    const cityName = cityList[i];
+    const result = `My city Name is ${cityName.name} And code number ${cityName.code}`;
+    console.log(result);
+ }
+ */
+
+//  for of loop use
+/*
+for(let item of cityList){
+    const city = `My City Name is ${item.name} & Code Number ${item.code}`;
+    console.log(city);
+};
+*/
